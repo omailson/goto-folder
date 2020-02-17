08088f1b266353ae0c2bc19bd00bb97575ff78b0
Mailson Menezes <mailson@gmail.com>
2020-02-17 16:55:04 -0300

    Reorganize tests

--- gotofolder/test_resolvers.py
+++ gotofolder/test_resolvers.py
@@ -1,7 +1,7 @@
 import os
 
 from gotofolder.helpers import Path
-from gotofolder.resolvers import FileResolver, EnvVarResolver, DictResolver
+from gotofolder.resolvers import FileResolver, EnvVarResolver, DictResolver, BaseResolver
 
 TESTDATA_DIR = "testdata"
 TESTENVVAR = "MOCK_GOTOFOLDERS"
@@ -24,12 +24,33 @@ def path_to(path):
     return os.path.abspath(os.path.join(TESTDATA_DIR, path))
 
 
+def assert_key_error(resolver: BaseResolver, key: str):
+    try:
+        _ = resolver[key]
+        assert False
+    except KeyError:
+        assert True
+
+
 class TestResolvers:
-    def test_envvarresolver_pass_to_the_next_resolver(self):
-        resolver = EnvVarResolver(TESTENVVAR, next_resolver=DictResolver({"foo": "bar"}))
+    # Test base resolver
+    def test_unknown_bookmark(self):
+        resolver = create_resolver_in()
 
-        assert resolver["foo"] == "bar"
+        assert_key_error(resolver, "x")
 
+    def test_slashes_are_ignored(self):
+        resolver = create_resolver_in()
+
+        assert_key_error(resolver, "a/c")
+
+    def test_dots_are_ignored(self):
+        key = "foo.bar"
+        resolver = DictResolver({key: "bar"})
+
+        assert_key_error(resolver, key)
+
+    # Test file resolver
     def test_simple_resolve(self):
         resolver = create_resolver_in()
 
@@ -40,24 +61,6 @@ class TestResolvers:
         expected_path2 = path_to("a/b/c")
         path_to_abc = resolver["abc"]
         assert path_to_abc == expected_path2
-
-    def test_unknown_bookmark(self):
-        resolver = create_resolver_in()
-
-        try:
-            _ = resolver["x"]
-            assert False
-        except KeyError:
-            assert True
-
-    def test_slashes_are_ignored(self):
-        resolver = create_resolver_in()
-
-        try:
-            _ = resolver["a/c"]
-            assert False
-        except KeyError:
-            assert True
 
     def test_resolve_in_subfolder(self):
         resolver = create_resolver_in("a")
@@ -77,3 +80,9 @@ class TestResolvers:
 
         # Assert use of parent .goto
         assert resolver["abc"] == path_to_abc
+
+    # Test envvar resolver
+    def test_envvarresolver_pass_to_the_next_resolver(self):
+        resolver = EnvVarResolver(TESTENVVAR, next_resolver=DictResolver({"foo": "bar"}))
+
+        assert resolver["foo"] == "bar"
